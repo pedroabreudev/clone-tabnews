70eb8db82da1081163bc569c4bdc937aec26a03b
Pedro Henrique <85113076+pedroabreudev@users.noreply.github.com>
2024-12-28 17:00:50 +0000

    adicionando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
-function Home(){
-    return <h1>Oiii Nathaly, feliz natal! 😎</h1>
-}
+function Home() {
+    return (
+      <div>
+        <h1>Oiii Nathaly 😎</h1>
+  
+        <h2>
+          Eu sonho em viver tudo o que o Senhor tem planejado para nossas vidas, todos os propósitos.
+          <br />
+          Eu imagino a gente vivendo juntos, acordando todos os dias, buscando a presença de Deus em nosso lar, passando por situações, mesmo que difíceis, mas com Deus ajudando em cada uma delas.
+          <br />
+          Eu imagino a gente viajando juntos, realizando sonhos e conquistas juntinhos, tendo um lar abençoado e cheio da presença de Deus.
+          <br />
+          Aqueles videozinhos de bebês ouvindo a palavra de Deus através de histórias da Bíblia, eu fico imaginando a gente vivendo tudo isso 🥰☺️.
+          <br />
+          Eu gostaria de sermos juntos uma referência espiritual e familiar, que a nossa forma de vivermos juntos seja o reflexo do amor de Deus 🥰🥰.
+          <br />
+          Eu me comprometo a buscar todos os dias ☺️🙏🏻.
+          <br />
+          Eu me comprometo também a te amar, te respeitar, honrar e me entregar assim como Cristo amou a igreja. A buscar a cada dia ser parecido com Ele, aprender desse amor e derramar sobre a sua vida.
+          <br />
+          Eu creio que é normal a gente ficar sem assunto, às vezes eles se esgotam, a rotina pode dificultar, e eu acho que é tudo bem ☺️.
+        </h2>
+      </div>
+    );
+  }
 
 export default Home;
